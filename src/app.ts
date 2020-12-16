1dbe4cb488dac6483a83ed21a44f09086b55695b
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2020-12-16 15:28:47 +0000

    Modify app entry point

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,42 @@
-import * as express from "express";
+import express from "express";
 import * as nunjucks from "nunjucks";
 import * as path from "path";
+import cookieParser from "cookie-parser";
+import Redis from "ioredis";
+
+import { 
+  SessionStore, 
+  SessionMiddleware, 
+  CookieConfig 
+} from "@companieshouse/node-session-handler";
+
+import { 
+  createLoggerMiddleware 
+} from "@companieshouse/structured-logging-node";
+
+import { 
+  serverErrorHandler,
+  authentication
+} from "./controller";
 
 import router from "./routers";
-import { serverErrorHandler } from "./controller";
+
+import {
+  PIWIK_SITE_ID,
+  PIWIK_URL,
+  COOKIE_SECRET,
+  COOKIE_DOMAIN,
+  CACHE_SERVER,
+  APPLICATION_NAME,
+  SCOTTISH_BANKRUPT_OFFICER,
+  SCOTTISH_BANKRUPT_OFFICER_DETAILS
+} from "./config";
 
 const app = express();
 
-// set some app variables from the environment
-app.set("port", process.env.PORT || "3000");
-app.set("dev", process.env.NODE_ENV === "development");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
 
 // where nunjucks templates should resolve to
 const viewPath = path.join(__dirname, "views");
@@ -24,12 +51,19 @@ const env = nunjucks.configure([
   express: app,
 });
 
+const cookieConfig: CookieConfig = { cookieName: "__SID", cookieSecret: COOKIE_SECRET, cookieDomain: COOKIE_DOMAIN };
+const sessionStore = new SessionStore(new Redis(`redis://${CACHE_SERVER}`));
+
+app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], createLoggerMiddleware(APPLICATION_NAME));
+app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], SessionMiddleware(cookieConfig, sessionStore));
+app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], authentication);
+
 app.set("views", viewPath);
 app.set("view engine", "html");
 
 // add global variables to all templates
-env.addGlobal("PIWIK_URL", "https://example.com");
-env.addGlobal("PIWIK_SITE_ID", "123");
+env.addGlobal("PIWIK_URL", PIWIK_URL);
+env.addGlobal("PIWIK_SITE_ID", PIWIK_SITE_ID);
 
 // serve static assets in development. this will not execute in production.
 if (process.env.NODE_ENV === "development") {
