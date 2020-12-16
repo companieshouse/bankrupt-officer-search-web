3e3b85fbd1224158e35304ea19c16bdc8cef79fe
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2020-12-16 12:30:31 +0000

    Add configuration environment

---
+++ src/config/index.ts
@@ -0,0 +1,23 @@
+const getEnvironmentValue = (key: string, defaultValue?: any): string => {
+    const isMandatory: boolean = !defaultValue;
+    const value: string = process.env[key] || "";
+
+    if (!value && isMandatory) {
+        throw new Error(`Please set the environment variable "${key}"`);
+    }
+
+    return value || defaultValue as string;
+};
+
+export const PIWIK_URL = getEnvironmentValue("PIWIK_URL");
+export const PIWIK_SITE_ID = getEnvironmentValue("PIWIK_SITE_ID");
+export const COOKIE_SECRET = getEnvironmentValue("COOKIE_SECRET");
+export const CACHE_SERVER = getEnvironmentValue("CACHE_SERVER");
+export const API_URL = getEnvironmentValue("API_URL");
+export const CHS_URL = getEnvironmentValue("CHS_URL");
+export const API_KEY = getEnvironmentValue("CHS_API_KEY");
+export const COOKIE_DOMAIN = getEnvironmentValue("COOKIE_DOMAIN");
+
+export const APPLICATION_NAME = "bankrupt-officer-search-web";
+export const SCOTTISH_BANKRUPT_OFFICER = "/admin/officer-search/scottish-bankrupt-officer";
+export const SCOTTISH_BANKRUPT_OFFICER_DETAILS = "/admin/officer-search/scottish-bankrupt-officer/:id";
