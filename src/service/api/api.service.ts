e9391334fe799b63c09d23ea90508ff80feec3c1
mikecsmith <msmith1@companieshouse.gov.uk>
2020-12-22 00:33:14 +0000

    Add fetchBankruptOfficer function

--- src/service/api/api.service.ts
+++ src/service/api/api.service.ts
@@ -4,6 +4,16 @@ import { logger } from '../../utils'
 import { ORACLE_QUERY_API_URL, ORACLE_QUERY_API_ENDPOINT } from '../../config'
 import { BankruptOfficerSearchFilters, BankruptOfficerSearchQuery, BankruptOfficerSearchResults } from '../../types'
 
+export const fetchBankruptOfficer = async (ephemeralKey: string): Promise<AxiosResponse<any>> => {
+  try {
+    const queryURL = `${ORACLE_QUERY_API_URL}${ORACLE_QUERY_API_ENDPOINT}/${ephemeralKey}`
+    return await axios.get(queryURL)
+  } catch (err) {
+    logger.error(err)
+    throw createError(err)
+  }
+}
+
 export const fetchBankruptOfficers = async (query: BankruptOfficerSearchQuery): Promise<AxiosResponse<BankruptOfficerSearchResults>> => {
   try {
     const queryURL = `${ORACLE_QUERY_API_URL}${ORACLE_QUERY_API_ENDPOINT}`
--- src/service/index.ts
+++ src/service/index.ts
@@ -2,5 +2,6 @@
  *
  */
 
+export { fetchBankruptOfficer } from './api/api.service'
 export { fetchBankruptOfficers } from './api/api.service'
 export { generateQuery } from './api/api.service'
