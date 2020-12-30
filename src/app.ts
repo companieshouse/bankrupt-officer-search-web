616ffbe1e025fa578296b2f27f25973b78f6777e
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2020-12-30 07:30:52 +0000

    Remove unused global variables and reintroduce semicolons

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
-import express from 'express'
-import * as nunjucks from 'nunjucks'
-import * as path from 'path'
-import cookieParser from 'cookie-parser'
-import Redis from 'ioredis'
-import helmet from 'helmet'
+import express from 'express';
+import * as nunjucks from 'nunjucks';
+import * as path from 'path';
+import cookieParser from 'cookie-parser';
+import Redis from 'ioredis';
+import helmet from 'helmet';
 
 import {
   SessionStore,
   SessionMiddleware,
   CookieConfig
-} from '@companieshouse/node-session-handler'
+} from '@companieshouse/node-session-handler';
 
 import {
   createLoggerMiddleware
-} from '@companieshouse/structured-logging-node'
+} from '@companieshouse/structured-logging-node';
 
 import {
   serverErrorHandler,
   authentication
-} from './controller'
+} from './controller';
 
-import router from './routers'
+import router from './routers';
 
 import {
   PIWIK_SITE_ID,
@@ -32,13 +32,13 @@ import {
   APPLICATION_NAME,
   SCOTTISH_BANKRUPT_OFFICER,
   SCOTTISH_BANKRUPT_OFFICER_DETAILS
-} from './config'
+} from './config';
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
@@ -57,10 +57,10 @@ app.use(helmet({
       objectSrc: ["'none'"]
     }
   }
-}))
+}));
 
 // where nunjucks templates should resolve to
-const viewPath = path.join(__dirname, 'views')
+const viewPath = path.join(__dirname, 'views');
 
 // set up the template engine
 const env = nunjucks.configure([
@@ -70,38 +70,27 @@ const env = nunjucks.configure([
 ], {
   autoescape: true,
   express: app
-})
+});
 
-const cookieConfig: CookieConfig = { cookieName: '__SID', cookieSecret: COOKIE_SECRET, cookieDomain: COOKIE_DOMAIN }
-const sessionStore = new SessionStore(new Redis(`redis://${CACHE_SERVER}`))
+const cookieConfig: CookieConfig = { cookieName: '__SID', cookieSecret: COOKIE_SECRET, cookieDomain: COOKIE_DOMAIN };
+const sessionStore = new SessionStore(new Redis(`redis://${CACHE_SERVER}`));
 
-app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], createLoggerMiddleware(APPLICATION_NAME))
-app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], SessionMiddleware(cookieConfig, sessionStore))
-app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], authentication)
+app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], createLoggerMiddleware(APPLICATION_NAME));
+app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], SessionMiddleware(cookieConfig, sessionStore));
+app.use([SCOTTISH_BANKRUPT_OFFICER, SCOTTISH_BANKRUPT_OFFICER_DETAILS], authentication);
 
-app.set('views', viewPath)
-app.set('view engine', 'html')
+app.set('views', viewPath);
+app.set('view engine', 'html');
 
 // add global variables to all templates
-env.addGlobal('PIWIK_URL', PIWIK_URL)
-env.addGlobal('PIWIK_SITE_ID', PIWIK_SITE_ID)
-env.addGlobal('CDN_HOST', CDN_HOST)
-
-// serve static assets in development. this will not execute in production.
-if (process.env.NODE_ENV !== 'production') {
-  app.use('/static', express.static('dist/app/static'))
-  env.addGlobal('CSS_URL', '/static/app.css')
-  env.addGlobal('FOOTER', '/static/footer.css')
-} else {
-  app.use('/static', express.static('static'))
-  env.addGlobal('CSS_URL', '/static/app.css')
-  env.addGlobal('FOOTER', '/static/footer.css')
-}
+env.addGlobal('PIWIK_URL', PIWIK_URL);
+env.addGlobal('PIWIK_SITE_ID', PIWIK_SITE_ID);
+env.addGlobal('CDN_HOST', CDN_HOST);
 
 // apply our default router to /
-app.use('/', router)
+app.use('/', router);
 
 // set up error handler
-app.use(serverErrorHandler)
+app.use(serverErrorHandler);
 
-export default app
+export default app;
