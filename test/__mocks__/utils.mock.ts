86dea5bdb878015a4129791d129e9b5de9d2ea5c
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2020-12-30 11:42:38 +0000

    Remove exporting stubbed variables from external files to better maintaine/secure test environment and rename file

--- test/__mocks__/utils.mock.ts
+++ test/__mocks__/utils.mock.ts
@@ -1,21 +1,12 @@
-import sinon = require("sinon");
-
 import { SCOTTISH_BANKRUPT_OFFICER } from "../../src/config";
 import { getSessionRequest } from "./session.mock";
 
 export const PAGE_NOT_FOUND = "page not found";
 export const LINK_EXPIRED = "This link has expired";
 export const SERVER_ERROR = "Sorry, there is a problem with the service";
+export const EPHEMERALKEY = "B687FDB8F0E171DBE05400144FFBDD12";
 
 export const response = { } as unknown as Response;
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const mockResponse = () => {
-  const res = { render: {}, status: {} };
-  res.status = sinon.stub().returns(res);
-  res.render = sinon.stub().returns(res);
-  return res;
-};
 
 export const mockRequest = { 
   path: SCOTTISH_BANKRUPT_OFFICER,
@@ -29,5 +20,3 @@ export const correctPathRequest = {
 export const FAKE_URL = { 
   path: "tryAgainNoLuck" 
 };
-
-export const mockNext = sinon.stub();
