9ff2e6d801ca1168996d0408adf741172ede320d
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2020-12-31 12:14:10 +0000

    Add unit test and error handler on bankrupt-officer controller, remove boilerplate code and reintroduce semicolons

---
+++ test/controller/bankrupt-officer.spec.ts
@@ -0,0 +1,84 @@
+import { NextFunction, Request, Response } from 'express';
+import chai, { expect } from 'chai';
+import sinonChai from "sinon-chai";
+import sinon from "sinon";
+import axios from 'axios';
+
+import { bankruptOfficer } from "../../src/controller";
+import { logger } from '../../src/utils';
+
+import { 
+  mockAxiosResponse, 
+  statusCode 
+} from '../__mocks__/utils.mock';
+
+chai.use(sinonChai);
+
+const nextFunctionSpy: NextFunction = sinon.spy();
+const mockResponse = () => {
+  const res = { render : {}, status : {}};
+  res.render = sinon.stub().returns(res);
+  res.status = sinon.stub().returns(res);
+  return res as unknown as Response;
+};
+const req = { } as unknown as Request;
+let res: Response;
+
+describe('BankruptOfficerController test suite', () => {
+
+  beforeEach(done => {
+    sinon.reset();
+    sinon.restore();
+    
+    res = mockResponse();
+    sinon.stub(logger, 'error').returns();
+    done();
+  });
+    
+  afterEach(done => {
+    sinon.reset();
+    sinon.restore();
+    done();
+  });
+
+  it('should return bankruptOfficer with data object and render bankrupt_officer page', async () => {
+    sinon.stub(axios, 'get').resolves(mockAxiosResponse.ok);
+
+    await bankruptOfficer(req, res, nextFunctionSpy);
+
+    expect(nextFunctionSpy).not.called;
+    expect(res.render).to.have.been.calledOnce;
+    expect(res.render).to.have.been.calledWith('bankrupt_officer', { bankruptOfficer: "ok" });
+  });
+
+  it('should return bankruptOfficer with status code 500 and render error-pages/500 page', async () => {
+    sinon.stub(axios, 'get').rejects(mockAxiosResponse.server_error);
+
+    await bankruptOfficer(req, res, nextFunctionSpy);
+
+    expect(nextFunctionSpy).not.called;
+    expect(res.status).to.have.been.calledWith(statusCode.server_error);
+    expect(res.render).to.have.been.calledWith('error-pages/500');
+  });
+
+  it('should return bankruptOfficer with status code 404 and render error-pages/404-link-expired page', async () => {
+    sinon.stub(axios, 'get').rejects(mockAxiosResponse.client_error);
+
+    await bankruptOfficer(req, res, nextFunctionSpy);
+
+    expect(nextFunctionSpy).not.called;
+    expect(res.status).to.have.been.calledWith(statusCode.client_error);
+    expect(res.render).to.have.been.calledWith('error-pages/404-link-expired');
+  });
+
+  it('should catch any error and call next function', async () => {
+    const resThrowsToBeCatched = { render: sinon.stub().throws() } as unknown as Response;
+        
+    await bankruptOfficer(req, resThrowsToBeCatched, nextFunctionSpy);
+  
+    expect(nextFunctionSpy).to.have.been.calledOnce;
+    expect(res.status).not.called;
+    expect(res.render).not.called;
+  });
+
+});
