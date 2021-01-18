46633f4434923e09589cbc7a5548cd2a3228e2e2
mouhajer-ch <mzaouali@companieshouse.gov.uk>
2021-01-18 14:23:19 +0000

    Add permissions check for BADOs user role

--- src/controller/authentication/authentication.controller.ts
+++ src/controller/authentication/authentication.controller.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { createLogger } from '@companieshouse/structured-logging-node';
 
-import { APPLICATION_NAME, SCOTTISH_BANKRUPT_OFFICER } from '../../config';
-import { userSession } from "../../utils";
+import {
+  APPLICATION_NAME,
+  SCOTTISH_BANKRUPT_OFFICER
+} from '../../config';
+
+import {
+  userSession
+} from "../../utils";
 
 const logger = createLogger(APPLICATION_NAME);
 
@@ -11,12 +17,18 @@ export default (req: Request, res: Response, next: NextFunction): void => {
     const signedIn = userSession.checkUserSignedIn(req.session);
 
     if (!signedIn) {
-      logger.info('User unauthorized, status_code=401, redirecting to sign in page');
+      logger.info('User non authenticated, status_code=401, redirecting to sign in page');
       return res.redirect(`/signin?return_to=${SCOTTISH_BANKRUPT_OFFICER}`);
     } 
+
+    if(userSession.checkPermission(req.session)){
+      logger.info(`User (${userSession.getLoggedInUserEmail(req.session)}) is signed in`);
+      next();
+    } else {
+      logger.info('User authenticated but non authorized (no permission), status_code=404');
+      return res.status(404).render('error-pages/404');
+    }
     
-    logger.info(`User (${userSession.getUserId(req.session)}) is signed in`);
-    next();
   } catch (err) {
     logger.error(err);
     next(err);
