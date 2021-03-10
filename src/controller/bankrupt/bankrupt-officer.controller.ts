9743f1acf35bb033fdc090f343096ba9c30c4d06
gevans-ch <gevans@companieshouse.gov.uk>
2021-03-10 15:02:47 +0000

    Added logic for user sign out feature on pages

--- src/controller/bankrupt/bankrupt-officer.controller.ts
+++ src/controller/bankrupt/bankrupt-officer.controller.ts
@@ -1,18 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import { fetchBankruptOfficer } from '../../service';
-import { logger, formattingOfficersInfo } from '../../utils';
+import { logger, formattingOfficersInfo, userSession} from '../../utils';
 
 export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const bankruptOfficer = await fetchBankruptOfficer(req.params?.id);
+      const bankruptOfficer = await fetchBankruptOfficer(req.params?.id);
+      const userEmail = userSession.getLoggedInUserEmail(req.session);
+      
+      console.log(userSession.getLoggedInUserEmail(req.session));
 
-    if(!bankruptOfficer.error){
-      const officer = (bankruptOfficer.data) ? formattingOfficersInfo([bankruptOfficer.data])[0] : {};
-      return res.render('bankrupt_officer', { bankruptOfficer: officer });
+      if (!bankruptOfficer.error) {
+          const officer = (bankruptOfficer.data) ? formattingOfficersInfo([bankruptOfficer.data])[0] : {};
+          return res.render('bankrupt_officer', { bankruptOfficer: officer , userEmail } );
     } else if( bankruptOfficer.status === 500) {
-      return res.status(bankruptOfficer.status).render('error-pages/500');
+        return res.status(bankruptOfficer.status).render('error-pages/500', { userEmail });
     } else {
-      return res.status(bankruptOfficer.status).render('error-pages/404-link-expired');
+      return res.status(bankruptOfficer.status).render('error-pages/404-link-expired', { userEmail });
     }
 
   } catch (err) {
--- src/controller/error/error.controller.ts
+++ src/controller/error/error.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
+import { userSession } from '../../utils'
 
 export function notFoundErrorHandler(req: Request, res: Response): void {
-  return res.status(404).render('error-pages/404');
+  const userEmail = userSession.getLoggedInUserEmail(req.session) 
+  return res.status(404).render('error-pages/404', { userEmail });
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function serverErrorHandler(err, req: Request, res: Response): void {
-  return res.status(err.statusCode || 500).render('error-pages/500');
+  const userEmail = userSession.getLoggedInUserEmail(req.session) 
+  return res.status(err.statusCode || 500).render('error-pages/500', { userEmail });
 }
