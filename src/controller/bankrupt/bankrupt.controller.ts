269c502c6a078cb2fdeaaa76aa4aa4256d73f6f7
dsanson <dsanson@companieshouse.gov.uk>
2022-10-14 10:16:39 +0100

    remove unecessary spacing changes from bankrupt officer controller

--- src/controller/bankrupt/bankrupt.controller.ts
+++ src/controller/bankrupt/bankrupt.controller.ts
@@ -4,7 +4,6 @@ import { logger, formattingOfficersInfo, userSession } from '../../utils';
 import { fetchBankruptOfficers } from '../../service';
 import { BankruptOfficerSearchFilters, BankruptOfficerSearchQuery } from '../../types';
 
-
 export const getSearchPage = (req: Request, res: Response, next: NextFunction): void => {
   try {
     const userEmail = userSession.getLoggedInUserEmail(req.session);
@@ -18,7 +17,7 @@ export const getSearchPage = (req: Request, res: Response, next: NextFunction): 
 export const postSearchPage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     // Get data from request body - dateOfBirth needs to be checked 
-    const { forename1 = '', surname = '', postcode = ''} = req.body;
+    const { forename1 = '', surname = '', postcode = '' } = req.body;
 
     // Deal with fragmented date of birth
     const dateOfBirth = 
@@ -26,11 +25,11 @@ export const postSearchPage = async (req: Request, res: Response, next: NextFunc
         `${req.body["dob-yyyy"]}-${req.body["dob-mm"]}-${req.body["dob-dd"]}` : '';
 
     // Set post query data
-    const filters: BankruptOfficerSearchFilters = { forename1, surname, dateOfBirth, postcode};
+    const filters: BankruptOfficerSearchFilters = { forename1, surname, dateOfBirth, postcode };
     const body: BankruptOfficerSearchQuery = { startIndex: 0, itemsPerPage: 10, filters};
 
     const results = await fetchBankruptOfficers(req.session, body);
-      // Not found officers has to be rendered anyway with an empty list 
+    // Not found officers has to be rendered anyway with an empty list 
    
     if(results.httpStatusCode === 404  || results.httpStatusCode === 200){
       const userEmail = userSession.getLoggedInUserEmail(req.session);
@@ -39,6 +38,7 @@ export const postSearchPage = async (req: Request, res: Response, next: NextFunc
     } else {
       return res.status(results.httpStatusCode).render('error-pages/500');
     } 
+    
   } catch (err) {
     logger.error(`${err}`);
     next(err);
