4668c14c7e4ec631d5b77ae5447d5a1c38ad66ee
hgurung1 <hgurung@companieshouse.gov.uk>
2022-11-15 14:30:16 +0000

    retain data

--- src/controller/bankrupt/bankrupt.controller.ts
+++ src/controller/bankrupt/bankrupt.controller.ts
@@ -17,7 +17,7 @@ export const getSearchPage = async (req: Request, res: Response, next: NextFunct
       return await renderSearchResultsPage(req, res, sessionExtraData.filters);
     } 
     const userEmail = userSession.getLoggedInUserEmail(req.session);
-    res.render('bankrupt', { userEmail });
+    res.render('bankrupt', { userEmail, filters: sessionExtraData?.filters });
   } catch (err) {
     logger.error(`${err}`);
     next(err);
@@ -27,19 +27,7 @@ export const getSearchPage = async (req: Request, res: Response, next: NextFunct
 export const postSearchPage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   
   try {
-    // Get data from request body - dateOfBirth needs to be checked 
-    const { forename1 = '', surname = '', alias = '', postcode = '' } = req.body;
-
-    // Deal with fragmented date of birth
-    const fromDateOfBirth = (req.body["from-dob-dd"] || req.body["from-dob-mm"] || req.body["from-dob-yyyy"]) ?
-      `${req.body["from-dob-yyyy"]}-${req.body["from-dob-mm"]}-${req.body["from-dob-dd"]}` : '';
-    
-    const toDateOfBirth = 
-      (req.body["to-dob-dd"] || req.body["to-dob-mm"] || req.body["to-dob-yyyy"]) ?
-        `${req.body["to-dob-yyyy"]}-${req.body["to-dob-mm"]}-${req.body["to-dob-dd"]}` : '';
-
-    // Set post query data
-    const filters: BankruptOfficerSearchFilters = { forename1, surname, alias, fromDateOfBirth, toDateOfBirth, postcode };
+    const filters: BankruptOfficerSearchFilters = generateFiltersFromBody(req);
     
     let sessionExtraData: undefined | BankruptOfficerSearchSessionExtraData = req.session?.getExtraData(BANKRUPT_OFFICER_SEARCH_SESSION);
     sessionExtraData = {...sessionExtraData, filters};
@@ -54,18 +42,18 @@ export const postSearchPage = async (req: Request, res: Response, next: NextFunc
     let toDobError: undefined | string = undefined;
     let whereTo: undefined | string = undefined;
 
-    if(isValidDate(fromDateOfBirth) === false && isValidDate(toDateOfBirth) === false){
+    if(isValidDate(filters.fromDateOfBirth) === false && isValidDate(filters.toDateOfBirth) === false){
       validationErrors.push(new ValidationError('invalidFromDob', 'Enter a valid date'));
       validationErrors.push(new ValidationError('invalidToDob', 'Enter a valid date'));
       const validationResult = new ValidationResult(validationErrors);
 
-      return res.render('bankrupt', { userEmail, validationResult, whereTo: "invalidFromDob", toDobError: "invalidToDob"});
-    } else if(fromDobInvalid(fromDateOfBirth)){
+      return res.render('bankrupt', { filters, userEmail, validationResult, whereTo: "invalidFromDob", toDobError: "invalidToDob"});
+    } else if(fromDobInvalid(filters.fromDateOfBirth)){
       validationErrors.push(new ValidationError('invalidFromDob', 'Enter a valid date'));
       whereTo = "invalidFromDob";
     }
 
-    if(toDobInvalid(toDateOfBirth,fromDateOfBirth)){
+    if(toDobInvalid(filters.toDateOfBirth,filters.fromDateOfBirth)){
       validationErrors.push(new ValidationError('invalidToDob', 'Enter a valid date'));
       toDobError = "invalidToDob";
     }
@@ -78,6 +66,7 @@ export const postSearchPage = async (req: Request, res: Response, next: NextFunc
     if (validationErrors.length > 0) {
       const validationResult = new ValidationResult(validationErrors);
       const templateVariables = {
+        filters,
         userEmail,
         validationResult,
         ...(whereTo && { whereTo }),
@@ -106,9 +95,25 @@ const renderSearchResultsPage = async (req: Request, res: Response, filters: Ban
       const numOfPages = Math.ceil(totalResults / RESULTS_PER_PAGE);
       paginationData = buildPaginationData(startIndex + 1, numOfPages, SCOTTISH_BANKRUPT_OFFICER);
     }
-    return res.render('bankrupt', { pagination: paginationData, itemsPerPage, startIndex, totalResults, items: formattingOfficersInfo(items), searched: true, userEmail });
+    return res.render('bankrupt', { filters, pagination: paginationData, itemsPerPage, startIndex, totalResults, items: formattingOfficersInfo(items), searched: true, userEmail });
   } else {
     return res.status(results.httpStatusCode).render('error-pages/500');
   } 
 };
 
+export const generateFiltersFromBody = (req: Request): BankruptOfficerSearchFilters => {
+  // Get data from request body - dateOfBirth needs to be checked 
+  const { forename1 = '', surname = '', alias = '', postcode = '' } = req.body;
+
+  // Deal with fragmented date of birth
+  const fromDateOfBirth = (req.body["from-dob-dd"] || req.body["from-dob-mm"] || req.body["from-dob-yyyy"]) ?
+    `${req.body["from-dob-yyyy"]}-${req.body["from-dob-mm"]}-${req.body["from-dob-dd"]}` : '';
+  
+  const toDateOfBirth = 
+    (req.body["to-dob-dd"] || req.body["to-dob-mm"] || req.body["to-dob-yyyy"]) ?
+      `${req.body["to-dob-yyyy"]}-${req.body["to-dob-mm"]}-${req.body["to-dob-dd"]}` : '';
+
+  // Set post query data
+  const filters: BankruptOfficerSearchFilters = { forename1, surname, alias, fromDateOfBirth, toDateOfBirth, postcode };
+  return filters;
+}
