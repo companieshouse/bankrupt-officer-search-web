9538ed142d331f3a06e0ca000c3a9e547080bb40
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-09-06 12:47:23 +0100

    CC-632 house keeping

--- src/controller/error/error.controller.ts
+++ src/controller/error/error.controller.ts
@@ -4,6 +4,6 @@ export function notFoundErrorHandler(req: Request, res: Response): void {
   return res.status(404).render('error-pages/404');
 }
 
-export function serverErrorHandler(err: {[key: string]: number}, req: Request, res: Response): void {
+export function serverErrorHandler(err: {[key: string]: number | undefined} | null, req: Request, res: Response): void {
   return res.status(err?.statusCode || 500).render('error-pages/500');
 }
