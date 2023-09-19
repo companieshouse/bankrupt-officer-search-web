19bc1f8c2fc9d47f28a7425b25ba1bad51a6e88a
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2023-09-19 18:14:12 +0100

    removing exec

--- docker_start.sh
+++ docker_start.sh
@@ -3,4 +3,4 @@
 npm i
 PORT=3000
 export NODE_PORT=${PORT}
-exec node /opt/server.js -- ${PORT}
+PORT="${PORT}" node /opt/server.js
--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -4,4 +4,4 @@
 
 PORT=3000
 export NODE_PORT=${PORT}
-exec node /opt/server.js -- ${PORT}
+PORT="${PORT}" node /opt/server.js
