6a8a784738a2c0e4e6d068306d4cea323b3eca06
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-02-26 09:35:43 +0000

    chore: remove debuggers and reset to previous state

--- src/service/api/api.service.ts
+++ src/service/api/api.service.ts
@@ -25,22 +25,9 @@ export const fetchBankruptOfficer = async (session: Session | undefined,
 export const fetchBankruptOfficers = async (session: Session | undefined, 
   query: BankruptOfficerSearchQuery): Promise<Resource<BankruptOfficerSearchResults>> => {
   const client = createOAuthApiClient(session);
-  return await client.badosService.getBankruptOfficers(query) 
+  return await client.badosService.getBankruptOfficers(query)
     .catch((e) => {
-      console.log('>>>>>>>>error: fetchBankruptOfficers');
-      console.log(e);
       logger.error(e);
-      if (e.httpStatusCode === 404) { // In case "404 Not found" throws an error
-        return {
-          httpStatusCode: e.httpStatusCode,
-          resource: {
-            itemsPerPage: 0,
-            startIndex: 0,
-            totalResults: 0,
-            items: []
-          }
-        };
-      }
       return e;
     });
 };
