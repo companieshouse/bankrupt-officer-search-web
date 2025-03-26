e824a3a2bbc7595180b6969032a6d7f896c603a1
iahmed-ch <iahmed@companieshouse.gov.uk>
2025-03-26 16:46:20 +0000

    upgrade tf modules to 1.0.296

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -20,7 +20,7 @@ terraform {
 }
 
 module "secrets" {
-  source      = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.236"
+  source      = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.296"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
@@ -29,7 +29,7 @@ module "secrets" {
 }
 
 module "ecs-service" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.236"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.296"
 
   # Environmental configuration
   environment             = var.environment
